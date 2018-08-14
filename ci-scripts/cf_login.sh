49dc064e5104a2d3c84360e7e10a13468afdcfbc
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-08-14 18:46:32 -0400

    login outside of conditional

--- ci-scripts/cf_login.sh
+++ ci-scripts/cf_login.sh
@@ -4,6 +4,18 @@ set -e
 CF_ORGANIZATION="gsa-18f-federalist"
 CF_API="https://api.fr.cloud.gov"
 
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+  CF_USERNAME=$CF_USERNAME_PRODUCTION
+  CF_PASSWORD=$CF_PASSWORD_PRODUCTION
+  CF_SPACE="production"
+elif [ "$CIRCLE_BRANCH" == "staging" ]; then
+  CF_USERNAME=$CF_USERNAME_STAGING
+  CF_PASSWORD=$CF_PASSWORD_STAGING
+  CF_SPACE="staging"
+else
+  echo "Current branch has no associated deployment. Exiting."
+  exit
+fi
 # install cf cli
 curl -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
 sudo dpkg -i cf-cli_amd64.deb
@@ -14,16 +26,5 @@ cf install-plugin autopilot -f -r CF-Community
 
 cf api $CF_API
 
-if [ "$CIRCLE_BRANCH" == "master" ]; then
-  echo "HELLO"
-  CF_SPACE="production"
-  cf login -u $CF_USERNAME_PRODUCTION -p $CF_PASSWORD_PRODUCTION -o $CF_ORGANIZATION -s $CF_SPACE
-elif [ "$CIRCLE_BRANCH" == "staging" ]; then
-  echo "WORLD"
-  CF_SPACE="staging"
-  cf login -u $CF_USERNAME_STAGING -p $CF_PASSWORD_STAGING -o $CF_ORGANIZATION -s $CF_SPACE
-else
-  echo "Current branch has no associated environment to rotate. Exiting."
-  exit
-fi
+cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 # cf logout
